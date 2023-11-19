108d5c0fca3d19f7ce0770aaf412fe3f5a3663af
Aleksandr Larin <alexsandr@larin.name>
2023-11-19 18:18:24 +0700

    Go executable

--- source/go/emaildup.go
+++ source/go/emaildup.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
 )
 
 func main() {
-	//fmt.Println("Hello Go!")
-	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	//})
-	//http.ListenAndServe(":9008", nil)
+	//https://twin.sh/articles/35/how-to-add-colors-to-your-console-terminal-output-in-go
+	t := time.Now()
+	colorGreen := "\033[32m"
+	colorWhite := "\033[97m"
+	colorGray := "\033[37m"
+	println()
+	println(colorGreen + "*************************************")
+	println("* E-mail Duplicator v. 2023-11-19   *")
+	println("* https://github.com/Yxine/EmailDup *")
+	println("*************************************" + colorWhite)
+	println()
+	if len(os.Args) < 2 {
+		println("  Command-line: EmailDup.exe file" + colorGray)
+		println()
+		return
+	}
+	fmt.Printf("  Start lines: %d\n", 0)
+	fmt.Printf("  Final lines: %d\n", 0)
+	defer func() {
+		fmt.Printf("  Time taken:  %v\n", time.Since(t))
+	}()
 }
